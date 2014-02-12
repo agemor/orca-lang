3c847e8181c688c065fcca706b118e00fdaa45e7
JongChan Choi <disjukr@naver.com>
2014-02-12 18:27:47 +0900

    machine

--- Elsa/elsa/vm/Machine.hx
+++ Elsa/elsa/vm/Machine.hx
@@ -1,7 +1,32 @@
 package elsa.vm;
 
+import haxe.ds.GenericStack;
+import haxe.ds.Vector;
+
 class Machine {
-	// TODO
+	public var stack: GenericStack<Data>;
+	public var register: Vector<Data>;
+	public var memory: Vector<Data>;
+	public var program: Array<Instruction>;
+	public var pointer: Int;
+	public var registerSize: Int;
+	public var memorySize: Int;
+	public function new(memorySize: Int, registerSize: Int) {
+		this.memorySize = memorySize;
+		this.registerSize = registerSize;
+	}
+	public function load(code: String) {
+		stack = new GenericStack<Data>();
+		register = new Vector<Data>(registerSize);
+		memory = new Vector<Data>(memorySize);
+		program = parse(code);
+	}
+	public function parse(code: String): Array<Instruction> {
+		return []; // TODO
+	}
+	public function run() {
+		// TODO
+	}
 }
 
 class Data {
