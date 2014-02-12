d8f0e91a55aa63d8fe723783d3a0eacc311acf4a
김현준 <agemor@naver.com>
2014-02-12 17:35:52 +0900

    구문 패턴 파일 추가

---
+++ Elsa/elsa/syntax/FunctionCallSyntax.hx
@@ -0,0 +1,12 @@
+package elsa.syntax;
+
+/**
+ * ...
+ * @author 김 현준
+ */
+class FunctionCallSyntax implements Syntax {
+
+	public function new() {
+		
+	}	
+}
---
+++ Elsa/elsa/syntax/InstanceSyntax.hx
@@ -0,0 +1,12 @@
+package elsa.syntax;
+
+/**
+ * ...
+ * @author 김 현준
+ */
+class InstanceSyntax implements Syntax {
+
+	public function new() {
+		
+	}	
+}
